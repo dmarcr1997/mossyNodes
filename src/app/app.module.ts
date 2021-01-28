90feb69cba30e20b77d4d5af1e3c81a1f216870e
Damon <dmarcr1997@gmail.com>
2021-01-28 16:14:45 -0600

    Add landing page skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
-import { CoursePageComponent } from './course-page/course-page.component';
-import { ResumePageComponent } from './resume-page/resume-page.component';
-import { AllContentPageComponent } from './all-content-page/all-content-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PortfolioPageComponent } from './components/portfolio-page/portfolio-page.component';
+import { CoursePageComponent } from './components/course-page/course-page.component';
+import { ResumePageComponent } from './components/resume-page/resume-page.component';
+import { AllContentPageComponent } from './components/all-content-page/all-content-page.component';
 
 @NgModule({
   declarations: [
