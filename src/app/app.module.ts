5592b8718490230c226c75cf65d01ef8b928cdfc
Damon <dmarcr1997@gmail.com>
2021-01-31 11:23:01 -0600

    Add course and all content skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PortfolioPageComponent } from './components/portfolio-page/portfolio-page.component';
 import { CoursePageComponent } from './components/course-page/course-page.component';
-import { ResumePageComponent } from './components/resume-page/resume-page.component';
 import { AllContentPageComponent } from './components/all-content-page/all-content-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AsideComponent } from './aside/aside.component';
@@ -19,7 +17,6 @@ import { AsideComponent } from './aside/aside.component';
     HomePageComponent,
     PortfolioPageComponent,
     CoursePageComponent,
-    ResumePageComponent,
     AllContentPageComponent,
     NavbarComponent,
     AsideComponent
