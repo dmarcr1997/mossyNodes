49db8911c0bb3c76390ca4c825b0b1c695aba798
Damon <dmarcr1997@gmail.com>
2021-02-11 21:19:51 -0600

    Created active link style change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  itemClass = ['nav-item', 'nav-item', 'nav-item', 'nav-item', 'nav-item'];
+  item1 = this.itemClass[0];
+  item2 = this.itemClass[1];
+  item3 = this.itemClass[2];
+  item4 = this.itemClass[3];
+  item5 = this.itemClass[4];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  navClick(activeNumber): void{
+    this.itemClass.map((value, index) => {
+      if(index === activeNumber){
+        value = 'nav-item active'
+      }
+      value = 'nav-item'
+    })
+  }
 }
