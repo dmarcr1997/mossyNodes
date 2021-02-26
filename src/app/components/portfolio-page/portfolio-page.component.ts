0c9a4516a42f1cc9dc2991ea240e1aca50b0fb6d
Damon <dmarcr1997@gmail.com>
2021-02-25 20:55:10 -0600

    Added projects to portfolio page

--- src/app/components/portfolio-page/portfolio-page.component.ts
+++ src/app/components/portfolio-page/portfolio-page.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as data from 'src/server/information.json';
 @Component({
   selector: 'app-portfolio-page',
   templateUrl: './portfolio-page.component.html',
   styleUrls: ['./portfolio-page.component.css']
 })
 export class PortfolioPageComponent implements OnInit {
+  projects
 
-  constructor() { }
+  constructor(){ 
+    this.projects = data.projects
+    console.log(this.projects[0]);
+  }
 
   ngOnInit(): void {
   }
